50af3102e36c609a1808a111b8b43e04cb21f0a6
nurbekovv05 <baevnurbol@gmail.com>
2023-05-16 17:21:22 +0600

    ds

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Navbar from "./components/Navbar";
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
+        <Routes>
+            <Route path='/' element={<Home/>} />
+        </Routes>
     </div>
   );
 }
